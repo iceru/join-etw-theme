803e66e691e1d67d5069f709da91280af8522162
Muhamad Hafiz <m.hafiz1825@gmail.com>
2026-05-09 12:14:45 +0700

    update pixel id

--- page-homepage.php
+++ page-homepage.php
@@ -11,6 +11,37 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('eco-fonts', 'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,600;1,300;1,400&family=Poppins:wght@300;400;500;600;700&display=swap', [], null);
 });
 
+add_action('wp_head', function () {
+    ?>
+    <!-- Meta Pixel Code -->
+    <script>
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s);
+        }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '3225326534321512');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=3225326534321512&ev=PageView&noscript=1" alt="" />
+    </noscript>
+    <!-- End Meta Pixel Code -->
+    <?php
+}, 20);
+
 get_header();
 ?>
 
